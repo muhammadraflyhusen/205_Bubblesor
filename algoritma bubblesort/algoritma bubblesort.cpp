0c91236a1115fd9afbfc5a0f3478b80ce529b17e
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-03-07 17:11:31 +0700

    membuat prosedur untuk input

--- algoritma bubblesort/algoritma bubblesort.cpp
+++ algoritma bubblesort/algoritma bubblesort.cpp
@@ -3,3 +3,40 @@ using namespace std;
 
 int arr[20];
 int n;
+
+
+void input() {
+    while (true)
+    {
+        cout << "masukan banyaknya elemen array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+
+} void bubbleSortArray() { //procedur untuk mengurutkan array
+    int pass = 1; //step 1
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+            if (arr[j] > arr[j + 1]) { //step 3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1; //step 4 
+    } while (pass < n - 1); //step 5
+} 
